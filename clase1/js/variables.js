12a63319c09484e54b5eb8cb3d5792af462822ad
Marcos <exegeses@gmail.com>
2024-11-09 18:05:01 -0300

    sintáxis de punto, controlador de ento "onclick", variables

---
+++ clase1/js/variables.js
@@ -0,0 +1,13 @@
+/*
+    Para declarar una variable en JavaScript 
+    utilizamos la palabra reservada "let"
+*/
+let numero = 10
+
+
+// Utilizamos el método write() para escribir en el documento
+document.write( numero )
+document.write( '<br>' )
+document.write( numero + 7 )
+
+
